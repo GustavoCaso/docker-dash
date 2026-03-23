1b6aa680d3e089c95ed88716d548d6a17edbbb14
Gustavo Caso <gustavocaso@gmail.com>
2026-03-23 08:21:41 +0100

    chore: Split Docker code into smaller file to make easier add functionality to the different services

---
+++ internal/client/docker_image_service.go
@@ -0,0 +1,117 @@
+package client
+
+import (
+	"context"
+	"log"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+)
+
+// Local Image Service.
+type imageService struct {
+	cli *client.Client
+}
+
+func (s *imageService) List(ctx context.Context) ([]Image, error) {
+	log.Printf("[docker] ImageList: all=true")
+	images, err := s.cli.ImageList(ctx, image.ListOptions{All: true})
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]Image, len(images))
+	for i, img := range images {
+		imageData, imageErr := s.get(ctx, img.ID)
+		if imageErr != nil {
+			return []Image{}, imageErr
+		}
+
+		imageData.Containers = img.Containers
+		result[i] = imageData
+	}
+
+	log.Printf("[docker] ImageList: returned count=%d", len(result))
+	return result, nil
+}
+
+// FetchLayers retrieves the layer history for an image.
+func (s *imageService) FetchLayers(ctx context.Context, imageID string) []Layer {
+	log.Printf("[docker] ImageHistory: id=%q", imageID)
+	history, err := s.cli.ImageHistory(ctx, imageID)
+	if err != nil {
+		return []Layer{}
+	}
+
+	layers := make([]Layer, 0, len(history))
+	for _, h := range history {
+		layers = append(layers, Layer{
+			ID:      h.ID,
+			Command: h.CreatedBy,
+			Size:    h.Size,
+			Created: timeFromUnix(h.Created),
+		})
+	}
+	slices.Reverse(layers)
+	log.Printf("[docker] ImageHistory: returned count=%d", len(layers))
+	return layers
+}
+
+const repoTagParts = 2 // parts when splitting repo:tag on ":"
+
+func (s *imageService) get(ctx context.Context, id string) (Image, error) {
+	img, err := s.cli.ImageInspect(ctx, id, client.ImageInspectWithManifests(true))
+	if err != nil {
+		return Image{}, err
+	}
+
+	repo := none
+	tag := none
+	if len(img.RepoTags) > 0 {
+		parts := strings.SplitN(img.RepoTags[0], ":", repoTagParts)
+		repo = parts[0]
+		if len(parts) > 1 {
+			tag = parts[1]
+		}
+	}
+
+	created, err := time.Parse(time.RFC3339Nano, img.Created)
+	if err != nil {
+		return Image{}, err
+	}
+
+	return Image{
+		ID:       img.ID,
+		Repo:     repo,
+		Tag:      tag,
+		Size:     img.Size,
+		Created:  created,
+		Dangling: len(img.RepoTags) == 0 || repo == none && tag == none,
+		Config:   img.Config,
+	}, nil
+}
+
+func (s *imageService) Remove(ctx context.Context, id string, force bool) error {
+	log.Printf("[docker] ImageRemove: id=%q force=%v", id, force)
+	_, err := s.cli.ImageRemove(ctx, id, image.RemoveOptions{Force: force})
+	log.Printf("[docker] ImageRemove: done err=%v", err)
+	return err
+}
+
+func (s *imageService) Prune(ctx context.Context, opts PruneOptions) (PruneReport, error) {
+	log.Printf("[docker] ImagesPrune: all=%v", opts.All)
+	f := filters.Args{}
+	if opts.All {
+		f = filters.NewArgs(filters.Arg("dangling", "false"))
+	}
+	r, err := s.cli.ImagesPrune(ctx, f)
+	if err != nil {
+		return PruneReport{}, err
+	}
+	log.Printf("[docker] ImagesPrune: deleted=%d spaceReclaimed=%d", len(r.ImagesDeleted), r.SpaceReclaimed)
+	return PruneReport{ItemsDeleted: len(r.ImagesDeleted), SpaceReclaimed: r.SpaceReclaimed}, nil
+}
